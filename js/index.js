79692bb7aa896f6a5f26f69b080a3b482e147afa
Leandro D'Ávila Martins Braga <leandrodavilamartins@gmail.com>
2022-02-12 23:37:04 -0300

    added tabela and pig files

--- js/index.js
+++ js/index.js
@@ -1,16 +1,21 @@
 
 const btn = document.getElementById('submitButton');
-const nome = document.getElementById('nome')
-const price = document.getElementById('price')
+const nome = document.getElementById('nome');
+const price = document.getElementById('price');
+const paragraph = document.getElementById('dbdata'); 
+const reqBtn = document.getElementById('reqBtn');
+
 
 btn.addEventListener('click', event => {
     event.preventDefault();
     submitForm(); // salva dados do formulário no firebase 
-    //console.log(nome.value);
-    //console.log(price.value)
 })
 
-function submitForm() {
+reqBtn.addEventListener('click', () =>{
+    reqData();
+})
+
+function submitForm() { // envia formulário para banco de dados 
     db.collection('cotacoes').doc().set({
         nome:nome.value,
         price:price.value,
@@ -18,9 +23,30 @@ function submitForm() {
     })
     .then(() => {
         console.log('Dados salvos com sucesso!');
+        let msg = document.getElementById('msg');
+        msg.classList.add('unmasked');
+
     })
     .catch((error) => {
         console.error('Ocorreu um erro ao salvar os dados : ', error);
     })
 }
+
+function reqData() { // requisita dados 
+    db.collection('estoque').get().then( res => {
+        let objs = [];
+        let text = "";
+        let docs = res.docs;
+        //paragraph.innerText = docs;
+        docs.forEach( doc => {
+            return objs.push(doc.data());
+        })
+        console.log(objs)
+        objs.forEach( obj => {
+            return text += JSON.stringify(obj);
+        })
+        paragraph.innerText = text;
+    })
+}
+
 
